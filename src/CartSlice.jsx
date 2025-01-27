d1e1e053372a8e6e8ec5996e832ce3629ad20ba8
tarunbilla <billatarun6@gmail.com>
2025-01-27 13:28:16 -0500

    Updated CartSlice.jsx

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,24 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+        const {name, image, cost} = action.payload;
+        const existingItem = state.items.find(item => item.name === name);
+        if(existingItem){
+            existingItem.quantity++;
+        }
+        else{
+            state.items({name, image, cost, quantity: 1});
+        }
     },
     removeItem: (state, action) => {
+        state.items = state.items.filter(item => item.name !== action.payload);
     },
     updateQuantity: (state, action) => {
-
-    
+        const {name, amount} = action.payload;
+        const existingItem = state.items.find(item => item.name === name);
+        if(existingItem){
+            existingItem.quantity = amount;
+        }
     },
   },
 });
--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -2,6 +2,8 @@ import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
 import { addItem } from './CartSlice';
+import { removeItem } from './CartSlice';
+import { updateQuantity } from './CartSlice';
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
